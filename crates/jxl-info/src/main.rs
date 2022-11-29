3bb3765fa182ba2f76522b41e8698aec5a02d578
Wonwoo Choi <chwo9843@gmail.com>
2022-11-29 22:27:40 +0900

    jxl-info: Add ICC decoding routine

--- crates/jxl-info/src/main.rs
+++ crates/jxl-info/src/main.rs
@@ -3,6 +3,14 @@ fn main() {
     let mut bitstream = jxl_bitstream::Bitstream::new(file);
     let bitstream = &mut bitstream;
     let headers = jxl_bitstream::read_bits!(bitstream, Bundle(jxl_bitstream::header::Headers)).expect("Failed to read headers");
-    dbg!(headers);
-    dbg!(std::mem::size_of::<jxl_bitstream::header::Headers>());
+    dbg!(&headers);
+
+    if headers.metadata.colour_encoding.want_icc {
+        let enc_size = jxl_bitstream::read_bits!(bitstream, U64).unwrap();
+        dbg!(enc_size);
+        dbg!(bitstream.global_pos());
+        let decoder = jxl_coding::Decoder::parse(bitstream, 41);
+        dbg!(decoder);
+        dbg!(bitstream.global_pos());
+    }
 }
