4af89ebf3d8c20a4b59e548b9b502f846fe7714c
Wonwoo Choi <chwo9843@gmail.com>
2023-02-21 17:50:29 +0900

    Optimize small-sized DCT

--- crates/jxl-render/src/dct.rs
+++ crates/jxl-render/src/dct.rs
@@ -2,6 +2,23 @@ fn dct(input: &[f32], output: &mut [f32], inverse: bool) {
     let n = input.len();
     assert!(n.is_power_of_two());
     assert!(output.len() == n);
+    if n <= 8 {
+        let f = match (n, inverse) {
+            (0, _) => return,
+            (1, false) => small_dct::<1, 4, false>,
+            (2, false) => small_dct::<2, 8, false>,
+            (4, false) => small_dct::<4, 16, false>,
+            (8, false) => small_dct::<8, 32, false>,
+            (1, true) => small_dct::<1, 4, true>,
+            (2, true) => small_dct::<2, 8, true>,
+            (4, true) => small_dct::<4, 16, true>,
+            (8, true) => small_dct::<8, 32, true>,
+            _ => unreachable!(),
+        };
+        f(input, output);
+        return;
+    }
+
     let mut real = vec![0.0f32; n * 4];
     let mut imag = vec![0.0f32; n * 4];
 
@@ -32,13 +49,7 @@ fn dct(input: &[f32], output: &mut [f32], inverse: bool) {
         }
     }
 
-    match n {
-        1 => small_fft_in_place::<4>(&mut real, &mut imag),
-        2 => small_fft_in_place::<8>(&mut real, &mut imag),
-        4 => small_fft_in_place::<16>(&mut real, &mut imag),
-        8 => small_fft_in_place::<32>(&mut real, &mut imag),
-        _ => fft_in_place(&mut real, &mut imag),
-    }
+    fft_in_place(&mut real, &mut imag);
 
     if inverse {
         for (i, o) in imag[..2 * n].chunks_exact(2).zip(output) {
@@ -47,6 +58,66 @@ fn dct(input: &[f32], output: &mut [f32], inverse: bool) {
     } else {
         let div = (2 * n) as f32;
         for (idx, (i, o)) in real[..n].iter().zip(output).enumerate() {
+            let scale = if idx == 0 { div.recip() } else { std::f32::consts::SQRT_2 / div };
+            *o = *i * scale;
+        }
+    }
+}
+
+fn small_dct<const N: usize, const N4: usize, const INV: bool>(input: &[f32], output: &mut [f32]) {
+    assert_eq!(N * 4, N4);
+    assert!(N.is_power_of_two());
+    assert!(input.len() == N);
+    assert!(output.len() == N);
+    assert!(N.trailing_zeros() <= 3);
+
+    let fft = match N {
+        0 => return,
+        1 => small_fft_in_place::<4>,
+        2 => small_fft_in_place::<8>,
+        4 => small_fft_in_place::<16>,
+        8 => small_fft_in_place::<32>,
+        _ => unreachable!(),
+    };
+
+    let mut real = [0.0f32; N4];
+    let mut imag = [0.0f32; N4];
+
+    if INV {
+        for (idx, (i, o)) in input.iter().zip(&mut real[..N]).enumerate() {
+            *o = if idx == 0 {
+                *i * 2.0
+            } else {
+                *i * std::f32::consts::SQRT_2
+            };
+        }
+        let (l, r) = real[..2 * N].split_at_mut(N);
+        for (i, o) in l[1..].iter().rev().zip(&mut r[1..]) {
+            *o = -*i;
+        }
+        let (l, r) = real.split_at_mut(2 * N);
+        for (i, o) in l.iter_mut().zip(r) {
+            *o = -*i;
+        }
+        small_reorder::<N4>(&real, &mut imag);
+        real.fill(0.0f32);
+    } else {
+        small_reorder::<N>(input, output);
+        for (idx, val) in output.iter().enumerate() {
+            real[2 * N + 2 * idx] = *val;
+            real[4 * N - 2 * idx - 1] = *val;
+        }
+    }
+
+    fft(&mut real, &mut imag);
+
+    if INV {
+        for (i, o) in imag[..2 * N].chunks_exact(2).zip(output) {
+            *o = i[1] / 4.0;
+        }
+    } else {
+        let div = (2 * N) as f32;
+        for (idx, (i, o)) in real[..N].iter().zip(output).enumerate() {
             let scale = if idx == 0 { div.recip() } else { std::f32::consts::SQRT_2 / div };
             *o = *i * scale;
         }
@@ -142,13 +213,13 @@ fn small_fft_in_place<const N: usize>(real: &mut [f32], imag: &mut [f32]) {
                 let cos = COS_SIN[j * skip];
                 let sin = COS_SIN[j * skip + 8];
 
+                let ur = real[k + j];
+                let ui = imag[k + j];
                 let r = real[k + m / 2 + j];
                 let i = imag[k + m / 2 + j];
                 // (a + ib) (cos + isin) = (a cos - b sin) + i(b cos + a sin)
                 let tr = r * cos - i * sin;
                 let ti = i * cos + r * sin;
-                let ur = real[k + j];
-                let ui = imag[k + j];
 
                 real[k + j] = ur + tr;
                 imag[k + j] = ui + ti;
@@ -169,6 +240,41 @@ fn reorder(input: &[f32], output: &mut [f32]) {
     for (idx, i) in input.iter().enumerate() {
         let target = idx.reverse_bits() >> shift_bits;
         output[target] = *i;
+    }
+}
+
+fn small_reorder<const N: usize>(input: &[f32], output: &mut [f32]) {
+    const fn compute_idx_map<const N: usize>() -> [usize; N] {
+        let bits = N.trailing_zeros();
+        let shift_bits = usize::BITS - bits;
+
+        let mut out = [0; N];
+        let mut idx = 0;
+        while idx < N {
+            out[idx] = idx.reverse_bits() >> shift_bits;
+            idx += 1;
+        }
+        out
+    }
+
+    const IDX_MAP: [&[usize]; 6] = [
+        &[0],
+        &[0, 1],
+        &compute_idx_map::<4>(),
+        &compute_idx_map::<8>(),
+        &compute_idx_map::<16>(),
+        &compute_idx_map::<32>(),
+    ];
+
+    assert!(N.is_power_of_two());
+    assert!(input.len() >= N);
+    assert!(output.len() >= N);
+    let bits = N.trailing_zeros() as usize;
+    assert!(bits < IDX_MAP.len());
+
+    let map = IDX_MAP[bits];
+    for (idx, i) in input.iter().enumerate() {
+        output[map[idx]] = *i;
     }
 }
 
