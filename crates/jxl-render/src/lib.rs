54cc852c289a941632668e4b6aefde6eddd26fda
Wonwoo Choi <chwo9843@gmail.com>
2023-03-10 18:35:55 +0900

    Add render cache

--- crates/jxl-render/src/lib.rs
+++ crates/jxl-render/src/lib.rs
@@ -1,4 +1,4 @@
-use std::{io::Read, collections::BTreeMap};
+use std::{io::Read, collections::{HashSet, HashMap}};
 
 use jxl_bitstream::{Bitstream, Bundle};
 use jxl_frame::{
@@ -8,7 +8,7 @@ use jxl_frame::{
     Frame,
     ProgressiveResult,
 };
-use jxl_grid::SimpleGrid;
+use jxl_grid::{Grid, SimpleGrid};
 use jxl_image::{Headers, ImageMetadata, ColourSpace};
 use jxl_modular::ChannelShift;
 
@@ -20,64 +20,34 @@ pub use error::{Error, Result};
 
 #[derive(Debug)]
 pub struct RenderContext<'a> {
-    image_header: &'a Headers,
-    frames: Vec<Frame<'a>>,
-    lf_frame: Vec<usize>,
-    reference: Vec<usize>,
-    loading_frame: Option<Frame<'a>>,
+    inner: ContextInner<'a>,
+    state: RenderState,
 }
 
 impl<'a> RenderContext<'a> {
     pub fn new(image_header: &'a Headers) -> Self {
         Self {
-            image_header,
-            frames: Vec::new(),
-            lf_frame: vec![usize::MAX; 4],
-            reference: vec![usize::MAX; 4],
-            loading_frame: None,
+            inner: ContextInner::new(image_header),
+            state: RenderState::new(),
         }
-    }
-
-    fn metadata(&self) -> &'a ImageMetadata {
-        &self.image_header.metadata
-    }
-
-    fn xyb_encoded(&self) -> bool {
-        self.image_header.metadata.xyb_encoded
-    }
-
-    fn preserve_current_frame(&mut self) {
-        let Some(frame) = self.loading_frame.take() else { return; };
-
-        let header = frame.header();
-        let idx = self.frames.len();
-        let is_last = header.is_last;
-
-        if !is_last && (header.duration == 0 || header.save_as_reference != 0) && header.frame_type != FrameType::LfFrame {
-            let ref_idx = header.save_as_reference as usize;
-            self.reference[ref_idx] = idx;
-        }
-        if header.lf_level != 0 {
-            let lf_idx = header.lf_level as usize - 1;
-            self.lf_frame[lf_idx] = idx;
-        }
-        self.frames.push(frame);
     }
 }
 
 impl RenderContext<'_> {
+    #[inline]
     pub fn width(&self) -> u32 {
-        self.image_header.size.width
+        self.inner.width()
     }
 
+    #[inline]
     pub fn height(&self) -> u32 {
-        self.image_header.size.height
+        self.inner.height()
     }
 }
 
 impl RenderContext<'_> {
     pub fn read_icc_if_exists<R: Read>(&mut self, bitstream: &mut Bitstream<R>) -> Result<()> {
-        if self.metadata().colour_encoding.want_icc {
+        if self.inner.metadata().colour_encoding.want_icc {
             tracing::info!("Image has ICC profile");
             let icc = jxl_color::icc::read_icc(bitstream)?;
 
@@ -92,54 +62,19 @@ impl RenderContext<'_> {
         &mut self,
         bitstream: &mut Bitstream<R>,
         progressive: bool,
-        mut region: Option<(u32, u32, u32, u32)>,
+        region: Option<(u32, u32, u32, u32)>,
     ) -> Result<ProgressiveResult> {
-        let image_header = self.image_header;
-
         loop {
-            let frame = match &mut self.loading_frame {
-                Some(frame) => frame,
-                slot => {
-                    let frame = Frame::parse(bitstream, image_header)?;
-                    *slot = Some(frame);
-                    slot.as_mut().unwrap()
-                },
-            };
-
-            let header = frame.header();
-            let is_last = header.is_last;
-            tracing::info!(
-                width = header.width,
-                height = header.height,
-                frame_type = format_args!("{:?}", header.frame_type),
-                upsampling = header.upsampling,
-                lf_level = header.lf_level,
-                "Decoding {}x{} frame", header.width, header.height
-            );
-
-            if let Some(region) = &mut region {
-                frame.adjust_region(region);
-            };
-            let filter = if region.is_some() {
-                Box::new(frame.crop_filter_fn(region)) as Box<dyn FnMut(&_, &_, _) -> bool>
-            } else {
-                Box::new(|_: &_, _: &_, _| true)
-            };
-
-            if header.frame_type.is_normal_frame() {
-                let result = frame.load_with_filter(bitstream, progressive, filter)?;
-                match result {
-                    ProgressiveResult::FrameComplete => frame.complete()?,
-                    result => return Ok(result),
-                }
-            } else {
-                frame.load_all(bitstream)?;
-                frame.complete()?;
+            let (result, frame) = self.inner.load_cropped_single(bitstream, progressive, region)?;
+            if result != ProgressiveResult::FrameComplete {
+                return Ok(result);
             }
 
+            let is_last = frame.header().is_last;
             let toc = frame.toc();
             let bookmark = toc.bookmark() + (toc.total_byte_size() * 8);
-            self.preserve_current_frame();
+            self.inner.preserve_current_frame();
+            self.state.preserve_current_frame();
             if is_last {
                 break;
             }
@@ -160,49 +95,97 @@ impl RenderContext<'_> {
 }
 
 impl RenderContext<'_> {
-    pub fn render_cropped(&self, region: Option<(u32, u32, u32, u32)>) -> Result<Vec<SimpleGrid<f32>>> {
-        let Some(target_frame) = self.frames
+    pub fn render_cropped(&mut self, region: Option<(u32, u32, u32, u32)>) -> Result<Vec<SimpleGrid<f32>>> {
+        let mut target = self.inner.frames
             .iter()
-            .chain(self.loading_frame.as_ref())
-            .find(|f| f.header().frame_type.is_normal_frame())
-            else {
-                panic!("No regular frame found");
-            };
+            .zip(&mut self.state.renders)
+            .find(|(f, _)| f.header().frame_type.is_normal_frame());
 
-        self.render_frame(target_frame, region)
+        let (f, cache) = if let Some((f, state)) = &mut target {
+            let cache = match state {
+                FrameRender::None => {
+                    **state = FrameRender::InProgress(RenderCache::new(f));
+                    let FrameRender::InProgress(cache) = state else { unreachable!() };
+                    cache
+                },
+                FrameRender::InProgress(cache) => {
+                    cache
+                },
+                FrameRender::Done(grid) => {
+                    return Ok(grid.clone());
+                },
+            };
+            (*f, cache)
+        } else {
+            match &self.inner.loading_frame {
+                Some(f) if f.header().frame_type.is_normal_frame() => {
+                    if f.data().lf_global.is_none() {
+                        return Err(Error::IncompleteFrame);
+                    }
+
+                    if self.state.loading_render_cache.is_none() {
+                        self.state.loading_render_cache = Some(RenderCache::new(f));
+                    }
+                    let Some(cache) = &mut self.state.loading_render_cache else {
+                        unreachable!()
+                    };
+                    (f, cache)
+                },
+                _ => panic!(),
+            }
+        };
+
+        let grid = self.inner.render_frame(f, cache, region)?;
+        if let Some((_, render)) = target {
+            *render = FrameRender::Done(grid.clone());
+        }
+        Ok(grid)
     }
 }
 
-impl<'f> RenderContext<'f> {
-    fn render_frame<'a>(&'a self, frame: &'a Frame<'f>, mut region: Option<(u32, u32, u32, u32)>) -> Result<Vec<SimpleGrid<f32>>> {
+impl<'f> ContextInner<'f> {
+    fn render_frame<'a>(
+        &'a self,
+        frame: &'a Frame<'f>,
+        cache: &mut RenderCache,
+        mut region: Option<(u32, u32, u32, u32)>,
+    ) -> Result<Vec<SimpleGrid<f32>>> {
         if let Some(region) = &mut region {
             frame.adjust_region(region);
         }
 
         let mut fb = match frame.header().encoding {
             Encoding::Modular => {
-                self.render_modular(frame, region)
+                self.render_modular(frame, cache, region)
             },
             Encoding::VarDct => {
-                self.render_vardct(frame, region)
+                self.render_vardct(frame, cache, region)
             },
         }?;
 
         let [a, b, c] = &mut fb;
         if frame.header().do_ycbcr {
             jxl_color::ycbcr::ycbcr_upsample([a, b, c], frame.header().jpeg_upsampling);
-            jxl_color::ycbcr::perform_inverse_ycbcr([a, b, c]);
         }
         if let Gabor::Enabled(weights) = frame.header().restoration_filter.gab {
             filter::apply_gabor_like([a, b, c], weights);
         }
         filter::apply_epf([a, b, c], &frame.data().lf_group, frame.header());
 
+        if frame.header().do_ycbcr {
+            jxl_color::ycbcr::perform_inverse_ycbcr([a, b, c]);
+        }
+
         let [a, b, c] = fb;
         Ok(vec![a, b, c])
     }
 
-    fn render_modular<'a>(&'a self, frame: &'a Frame<'f>, _region: Option<(u32, u32, u32, u32)>) -> Result<[SimpleGrid<f32>; 3]> {
+    fn render_modular<'a>(
+        &'a self,
+        frame: &'a Frame<'f>,
+        _cache: &mut RenderCache,
+        _region: Option<(u32, u32, u32, u32)>,
+    ) -> Result<[SimpleGrid<f32>; 3]> {
         let metadata = self.metadata();
         let xyb_encoded = self.xyb_encoded();
         let frame_header = frame.header();
@@ -282,7 +265,12 @@ impl<'f> RenderContext<'f> {
         Ok(fb_yxb)
     }
 
-    fn render_vardct<'a>(&'a self, frame: &'a Frame<'f>, region: Option<(u32, u32, u32, u32)>) -> Result<[SimpleGrid<f32>; 3]> {
+    fn render_vardct<'a>(
+        &'a self,
+        frame: &'a Frame<'f>,
+        cache: &mut RenderCache,
+        region: Option<(u32, u32, u32, u32)>,
+    ) -> Result<[SimpleGrid<f32>; 3]> {
         let span = tracing::span!(tracing::Level::TRACE, "RenderContext::render_vardct");
         let _guard = span.enter();
 
@@ -301,20 +289,23 @@ impl<'f> RenderContext<'f> {
 
         // Modular extra channels are already merged into GlobalModular,
         // so it's okay to drop PassGroup modular
-        let mut group_coeffs: BTreeMap<_, HfCoeff> = BTreeMap::new();
-        for (&(_, group_idx), group_pass) in &frame_data.group_pass {
+        for (&(pass_idx, group_idx), group_pass) in &frame_data.group_pass {
             if let Some(region) = region {
                 if !frame_header.is_group_collides_region(group_idx, region) {
                     continue;
                 }
             }
+            if !cache.coeff_merged.insert((pass_idx, group_idx)) {
+                continue;
+            }
 
             let hf_coeff = group_pass.hf_coeff.as_ref().unwrap();
-            group_coeffs
+            cache.group_coeffs
                 .entry(group_idx as usize)
                 .or_insert_with(HfCoeff::empty)
                 .merge(hf_coeff);
         }
+        let group_coeffs = &cache.group_coeffs;
 
         let quantizer = &lf_global_vardct.quantizer;
         let oim = &metadata.opsin_inverse_matrix;
@@ -326,32 +317,39 @@ impl<'f> RenderContext<'f> {
             return Err(Error::NotSupported("LF frame is not supported"));
         }
 
-        let dequantized_lf = frame_data.lf_group
+        let lf_group_it = frame_data.lf_group
             .iter()
             .filter(|(&lf_group_idx, _)| {
                 let Some(region) = region else { return true; };
                 frame_header.is_lf_group_collides_region(lf_group_idx, region)
-            })
-            .map(|(&lf_group_idx, data)| {
-                let lf_coeff = data.lf_coeff.as_ref().unwrap();
-                let hf_meta = data.hf_meta.as_ref().unwrap();
-                let mut dequant_lf = vardct::dequant_lf(frame_header, &lf_global.lf_dequant, quantizer, lf_coeff);
-                if !subsampled {
-                    vardct::chroma_from_luma_lf(&mut dequant_lf, &lf_global_vardct.lf_chan_corr);
-                }
-                (lf_group_idx as usize, (dequant_lf, hf_meta))
-            })
-            .collect::<BTreeMap<_, _>>();
+            });
+        let mut hf_meta_map = HashMap::new();
+        for (&lf_group_idx, data) in lf_group_it {
+            let lf_group_idx = lf_group_idx as usize;
+            hf_meta_map.insert(lf_group_idx, data.hf_meta.as_ref().unwrap());
+            cache.dequantized_lf
+                .entry(lf_group_idx)
+                .or_insert_with(|| {
+                    let lf_coeff = data.lf_coeff.as_ref().unwrap();
+                    // let hf_meta = data.hf_meta.as_ref().unwrap();
+                    let mut dequant_lf = vardct::dequant_lf(frame_header, &lf_global.lf_dequant, quantizer, lf_coeff);
+                    if !subsampled {
+                        vardct::chroma_from_luma_lf(&mut dequant_lf, &lf_global_vardct.lf_chan_corr);
+                    }
+                    dequant_lf
+                });
+        }
+        let dequantized_lf = &cache.dequantized_lf;
 
         let group_dim = frame_header.group_dim() as usize;
         let groups_per_row = frame_header.groups_per_row() as usize;
 
-        // key is (y, x) to match raster order
         let varblocks = group_coeffs
-            .into_iter()
+            .iter()
             .flat_map(|(group_idx, hf_coeff)| {
-                let lf_group_id = frame_header.lf_group_idx_from_group_idx(group_idx as u32) as usize;
-                let (lf_dequant, hf_meta) = dequantized_lf.get(&lf_group_id).unwrap();
+                let lf_group_id = frame_header.lf_group_idx_from_group_idx(*group_idx as u32) as usize;
+                let lf_dequant = dequantized_lf.get(&lf_group_id).unwrap();
+                let hf_meta = hf_meta_map.get(&lf_group_id).unwrap();
                 let x_from_y = &hf_meta.x_from_y;
                 let b_from_y = &hf_meta.b_from_y;
 
@@ -361,15 +359,15 @@ impl<'f> RenderContext<'f> {
                 let base_lf_top = (group_row % 8) * group_dim;
 
                 hf_coeff.data
-                    .into_iter()
-                    .map(move |((bx, by), coeff_data)| {
+                    .iter()
+                    .map(move |(&(bx, by), coeff_data)| {
                         let dct_select = coeff_data.dct_select;
-                        let y = vardct::dequant_hf_varblock(&coeff_data, 1, oim, quantizer, dequant_matrices, None);
-                        let x = vardct::dequant_hf_varblock(&coeff_data, 0, oim, quantizer, dequant_matrices, Some(frame_header.x_qm_scale));
-                        let b = vardct::dequant_hf_varblock(&coeff_data, 2, oim, quantizer, dequant_matrices, Some(frame_header.b_qm_scale));
+                        let y = vardct::dequant_hf_varblock(coeff_data, 1, oim, quantizer, dequant_matrices, None);
+                        let x = vardct::dequant_hf_varblock(coeff_data, 0, oim, quantizer, dequant_matrices, Some(frame_header.x_qm_scale));
+                        let b = vardct::dequant_hf_varblock(coeff_data, 2, oim, quantizer, dequant_matrices, Some(frame_header.b_qm_scale));
                         let mut yxb = [y, x, b];
 
-                        let (bw, bh) = coeff_data.dct_select.dct_select_size();
+                        let (bw, bh) = dct_select.dct_select_size();
                         let bw = bw as usize;
                         let bh = bh as usize;
                         let bx = bx * 8;
@@ -449,5 +447,167 @@ impl<'f> RenderContext<'f> {
         }
 
         Ok(fb_yxb)
+    }
+}
+
+#[derive(Debug)]
+struct ContextInner<'a> {
+    image_header: &'a Headers,
+    frames: Vec<Frame<'a>>,
+    lf_frame: Vec<usize>,
+    reference: Vec<usize>,
+    loading_frame: Option<Frame<'a>>,
+}
+
+impl<'a> ContextInner<'a> {
+    fn new(image_header: &'a Headers) -> Self {
+        Self {
+            image_header,
+            frames: Vec::new(),
+            lf_frame: vec![usize::MAX; 4],
+            reference: vec![usize::MAX; 4],
+            loading_frame: None,
+        }
+    }
+}
+
+impl<'a> ContextInner<'a> {
+    #[inline]
+    fn width(&self) -> u32 {
+        self.image_header.size.width
+    }
+
+    #[inline]
+    fn height(&self) -> u32 {
+        self.image_header.size.height
+    }
+
+    #[inline]
+    fn metadata(&self) -> &'a ImageMetadata {
+        &self.image_header.metadata
+    }
+
+    #[inline]
+    fn xyb_encoded(&self) -> bool {
+        self.image_header.metadata.xyb_encoded
+    }
+
+    fn preserve_current_frame(&mut self) {
+        let Some(frame) = self.loading_frame.take() else { return; };
+
+        let header = frame.header();
+        let idx = self.frames.len();
+        let is_last = header.is_last;
+
+        if !is_last && (header.duration == 0 || header.save_as_reference != 0) && header.frame_type != FrameType::LfFrame {
+            let ref_idx = header.save_as_reference as usize;
+            self.reference[ref_idx] = idx;
+        }
+        if header.lf_level != 0 {
+            let lf_idx = header.lf_level as usize - 1;
+            self.lf_frame[lf_idx] = idx;
+        }
+        self.frames.push(frame);
+    }
+}
+
+impl ContextInner<'_> {
+    fn load_cropped_single<R: Read>(
+        &mut self,
+        bitstream: &mut Bitstream<R>,
+        progressive: bool,
+        mut region: Option<(u32, u32, u32, u32)>,
+    ) -> Result<(ProgressiveResult, &Frame)> {
+        let image_header = self.image_header;
+
+        let frame = match &mut self.loading_frame {
+            Some(frame) => frame,
+            slot => {
+                let frame = Frame::parse(bitstream, image_header)?;
+                *slot = Some(frame);
+                slot.as_mut().unwrap()
+            },
+        };
+
+        let header = frame.header();
+        tracing::info!(
+            width = header.width,
+            height = header.height,
+            frame_type = format_args!("{:?}", header.frame_type),
+            upsampling = header.upsampling,
+            lf_level = header.lf_level,
+            "Decoding {}x{} frame", header.width, header.height
+        );
+
+        if let Some(region) = &mut region {
+            frame.adjust_region(region);
+        };
+        let filter = if region.is_some() {
+            Box::new(frame.crop_filter_fn(region)) as Box<dyn FnMut(&_, &_, _) -> bool>
+        } else {
+            Box::new(|_: &_, _: &_, _| true)
+        };
+
+        if header.frame_type.is_normal_frame() {
+            let result = frame.load_with_filter(bitstream, progressive, filter)?;
+            match result {
+                ProgressiveResult::FrameComplete => frame.complete()?,
+                result => return Ok((result, frame)),
+            }
+        } else {
+            frame.load_all(bitstream)?;
+            frame.complete()?;
+        }
+
+        Ok((ProgressiveResult::FrameComplete, frame))
+    }
+}
+
+#[derive(Debug)]
+struct RenderState {
+    renders: Vec<FrameRender>,
+    loading_render_cache: Option<RenderCache>,
+}
+
+impl RenderState {
+    fn new() -> Self {
+        Self {
+            renders: Vec::new(),
+            loading_render_cache: None,
+        }
+    }
+}
+
+impl RenderState {
+    fn preserve_current_frame(&mut self) {
+        if let Some(cache) = self.loading_render_cache.take() {
+            self.renders.push(FrameRender::InProgress(cache));
+        } else {
+            self.renders.push(FrameRender::None);
+        }
+    }
+}
+
+#[derive(Debug)]
+enum FrameRender {
+    None,
+    InProgress(RenderCache),
+    Done(Vec<SimpleGrid<f32>>),
+}
+
+#[derive(Debug)]
+struct RenderCache {
+    dequantized_lf: HashMap<usize, [Grid<f32>; 3]>,
+    group_coeffs: HashMap<usize, HfCoeff>,
+    coeff_merged: HashSet<(u32, u32)>,
+}
+
+impl RenderCache {
+    fn new(_frame: &Frame<'_>) -> Self {
+        Self {
+            dequantized_lf: HashMap::new(),
+            group_coeffs: HashMap::new(),
+            coeff_merged: HashSet::new(),
+        }
     }
 }
