983ca8c7e8bf48f84bf936eab4965df737d8042f
Eugene Vert <eugene.a.vert@gmail.com>
2023-04-11 16:25:28 +0300

    splines: Add quantized splines decoding

--- crates/jxl-frame/src/data/spline.rs
+++ crates/jxl-frame/src/data/spline.rs
@@ -1,15 +1,24 @@
-#![allow(unused_variables, unused_mut, dead_code)]
+#![allow(unused_variables, unused_mut, dead_code, clippy::needless_range_loop)]
 
 use std::io::Read;
 
-use jxl_bitstream::{Bitstream, Bundle};
+use jxl_bitstream::{unpack_signed, Bitstream, Bundle};
+use jxl_coding::Decoder;
 
 use crate::Result;
 
 #[derive(Debug)]
 pub struct Splines {
-    coords: Vec<(i32, i32)>,
+    pub splines: Vec<QuantSpline>,
+    start_points: Vec<(i32, i32)>,
     quant_adjust: i32,
+}
+
+#[derive(Debug, Default, Clone)]
+pub struct QuantSpline {
+    points_deltas: Vec<(i32, i32)>,
+    xyb_dct: [[i32; 32]; 3],
+    sigma_dct: [i32; 32],
 }
 
 impl<Ctx> Bundle<Ctx> for Splines {
@@ -19,8 +28,64 @@ impl<Ctx> Bundle<Ctx> for Splines {
         let mut decoder = jxl_coding::Decoder::parse(bitstream, 6)?;
         decoder.begin(bitstream)?;
 
-        let num_splines = decoder.read_varint(bitstream, 2)? + 1;
-        let mut last = (0i32, 0i32);
-        todo!()
+        let num_splines = (decoder.read_varint(bitstream, 2)? + 1) as usize;
+        
+        let mut start_points = vec![(0i32, 0i32); num_splines];
+        for i in 0..num_splines {
+            let mut x = decoder.read_varint(bitstream, 1)? as i32;
+            let mut y = decoder.read_varint(bitstream, 1)? as i32;
+            if i != 0 {
+                x = unpack_signed(x as u32) + start_points[i - 1].0;
+                y = unpack_signed(y as u32) + start_points[i - 1].1;
+            }
+            start_points[i].0 = x;
+            start_points[i].1 = y;
+        }
+
+        let quant_adjust = unpack_signed(decoder.read_varint(bitstream, 0)?);
+
+        let mut splines = vec![QuantSpline::new(); num_splines];
+        for spline in &mut splines {
+            spline.decode(&mut decoder, bitstream)?;
+        }
+
+        Ok(Self {
+            quant_adjust,
+            splines,
+            start_points,
+        })
     }
 }
+
+impl QuantSpline {
+    fn new() -> Self {
+        Self::default()
+    }
+
+    fn decode<R: Read>(
+        &mut self,
+        decoder: &mut Decoder,
+        bitstream: &mut Bitstream<R>,
+    ) -> Result<()> {
+        let num_points = decoder.read_varint(bitstream, 3)? as usize;
+        self.points_deltas.resize(num_points, (0, 0));
+
+        for cp in &mut self.points_deltas {
+            cp.0 = unpack_signed(decoder.read_varint(bitstream, 4)?);
+            cp.1 = unpack_signed(decoder.read_varint(bitstream, 4)?);
+        }
+        for color_dct in &mut self.xyb_dct {
+            for i in color_dct {
+                *i = unpack_signed(decoder.read_varint(bitstream, 5)?);
+            }
+        }
+        for i in &mut self.sigma_dct {
+            *i = unpack_signed(decoder.read_varint(bitstream, 5)?);
+        }
+        Ok(())
+    }
+
+}
+
+}
+
