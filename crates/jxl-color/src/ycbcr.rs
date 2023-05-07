8e38d22a5ba36ec731093921574b8834a2b82cd6
Wonwoo Choi <chwo9843@gmail.com>
2023-05-07 13:43:40 +0900

    jxl-color: Fix YCbCr conversion constants

--- crates/jxl-color/src/ycbcr.rs
+++ crates/jxl-color/src/ycbcr.rs
@@ -8,11 +8,11 @@ pub fn perform_inverse_ycbcr(fb_cbycr: [&mut SimpleGrid<f32>; 3]) {
 
     for ((r, g), b) in cb.iter_mut().zip(y).zip(cr) {
         let cb = *r;
-        let y = *g + 0.5;
+        let y = *g + 128.0 / 255.0;
         let cr = *b;
 
         *r = y + 1.402 * cr;
-        *g = y - 0.344016 * cb - 0.714136 * cr;
+        *g = y + (-0.114 * 1.772 / 0.587) * cb + (-0.299 * 1.402 / 0.587) * cr;
         *b = y + 1.772 * cb;
     }
 }
