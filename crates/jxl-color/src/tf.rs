4889fa268d1edc388a4d7a78bfb3360329860996
Wonwoo Choi <chwo9843@gmail.com>
2023-05-09 21:59:12 +0900

    jxl-color: Implement fast sRGB conversion

--- crates/jxl-color/src/tf.rs
+++ crates/jxl-color/src/tf.rs
@@ -7,14 +7,133 @@ pub fn linear_to_gamma(samples: &mut [f32], gamma: f32) {
 }
 
 /// Converts the linear samples with the sRGB transfer curve.
+// Fast linear to sRGB conversion, ported from libjxl.
 pub fn linear_to_srgb(samples: &mut [f32]) {
+    #[cfg(target_arch = "x86_64")]
+    {
+        if is_x86_feature_detected!("avx2") && is_x86_feature_detected!("fma") {
+            return unsafe { linear_to_srgb_avx2(samples) };
+        }
+    }
+
+    const POWTABLE_UPPER: [u8; 16] = [
+        0x00, 0x0a, 0x19, 0x26, 0x32, 0x41, 0x4d, 0x5c,
+        0x68, 0x75, 0x83, 0x8f, 0xa0, 0xaa, 0xb9, 0xc6,
+    ];
+    const POWTABLE_LOWER: [u8; 16] = [
+        0x00, 0xb7, 0x04, 0x0d, 0xcb, 0xe7, 0x41, 0x68,
+        0x51, 0xd1, 0xeb, 0xf2, 0x00, 0xb7, 0x04, 0x0d,
+    ];
+
     for s in samples {
-        let a = s.abs();
-        *s = if a <= 0.0031308f32 {
-            12.92 * a
-        } else {
-            a.powf(1.0 / 2.4).mul_add(1.055, -0.055)
-        }.copysign(*s);
+        let v = s.to_bits() & 0x7fff_ffff;
+        let v_adj = f32::from_bits((v | 0x3e80_0000) & 0x3eff_ffff);
+        let pow = 0.059914046f32;
+        let pow = pow * v_adj - 0.10889456;
+        let pow = pow * v_adj + 0.107963754;
+        let pow = pow * v_adj + 0.018092343;
+
+        let idx = ((v >> 23) - 118) as usize;
+        let mul = 0x4000_0000 | (u32::from(POWTABLE_UPPER[idx]) << 18) | (u32::from(POWTABLE_LOWER[idx]) << 10);
+
+        let v = f32::from_bits(v);
+        let small = v * 12.92;
+        let acc = pow * f32::from_bits(mul) - 0.055;
+
+        *s = if v <= 0.0031308 { small } else { acc }.copysign(*s);
+    }
+}
+
+#[cfg(target_arch = "x86_64")]
+#[target_feature(enable = "avx2")]
+#[target_feature(enable = "fma")]
+unsafe fn linear_to_srgb_avx2(samples: &mut [f32]) {
+    use std::arch::x86_64::*;
+
+    #[repr(align(16))]
+    struct Aligned([u8; 16]);
+
+    const POWTABLE_UPPER: Aligned = Aligned([
+        0x00, 0x0a, 0x19, 0x26, 0x32, 0x41, 0x4d, 0x5c,
+        0x68, 0x75, 0x83, 0x8f, 0xa0, 0xaa, 0xb9, 0xc6,
+    ]);
+    const POWTABLE_LOWER: Aligned = Aligned([
+        0x00, 0xb7, 0x04, 0x0d, 0xcb, 0xe7, 0x41, 0x68,
+        0x51, 0xd1, 0xeb, 0xf2, 0x00, 0xb7, 0x04, 0x0d,
+    ]);
+
+    let powtable_upper = _mm256_castps_si256(
+        _mm256_broadcast_ps(&*(POWTABLE_UPPER.0.as_ptr() as *const _))
+    );
+    let powtable_lower = _mm256_castps_si256(
+        _mm256_broadcast_ps(&*(POWTABLE_LOWER.0.as_ptr() as *const _))
+    );
+
+    let mut chunks = samples.chunks_exact_mut(8);
+    let sign_mask = _mm256_set1_ps(f32::from_bits(0x8000_0000));
+    for chunk in &mut chunks {
+        let v = _mm256_loadu_ps(chunk.as_ptr());
+        let sign = _mm256_and_ps(sign_mask, v);
+        let v = _mm256_andnot_ps(sign_mask, v);
+
+        let v_adj = _mm256_and_ps(
+            _mm256_set1_ps(f32::from_bits(0x3eff_ffff)),
+            _mm256_or_ps(_mm256_set1_ps(f32::from_bits(0x3e80_0000)), v),
+        );
+        let pow = _mm256_set1_ps(0.059914046);
+        let pow = _mm256_fmadd_ps(pow, v_adj, _mm256_set1_ps(-0.10889456));
+        let pow = _mm256_fmadd_ps(pow, v_adj, _mm256_set1_ps(0.107963754));
+        let pow = _mm256_fmadd_ps(pow, v_adj, _mm256_set1_ps(0.018092343));
+
+        let exp_idx = _mm256_sub_epi32(
+            _mm256_srai_epi32::<23>(_mm256_castps_si256(v)),
+            _mm256_set1_epi32(118),
+        );
+        let pow_upper = _mm256_shuffle_epi8(
+            powtable_upper,
+            exp_idx,
+        );
+        let pow_lower = _mm256_shuffle_epi8(
+            powtable_lower,
+            exp_idx,
+        );
+        let mul = _mm256_or_si256(
+            _mm256_or_si256(
+                _mm256_slli_epi32::<18>(pow_upper),
+                _mm256_slli_epi32::<10>(pow_lower),
+            ),
+            _mm256_set1_epi32(0x4000_0000),
+        );
+        let mul = _mm256_castsi256_ps(mul);
+
+        let small = _mm256_mul_ps(v, _mm256_set1_ps(12.92));
+        let acc = _mm256_fmadd_ps(pow, mul, _mm256_set1_ps(-0.055));
+
+        let mask = _mm256_cmp_ps(v, _mm256_set1_ps(0.0031308), _CMP_LE_OS);
+        let ret = _mm256_or_ps(
+            _mm256_and_ps(mask, small),
+            _mm256_andnot_ps(mask, acc),
+        );
+        let ret = _mm256_or_ps(ret, sign);
+        _mm256_storeu_ps(chunk.as_mut_ptr(), ret);
+    }
+
+    for s in chunks.into_remainder() {
+        let v = s.to_bits() & 0x7fff_ffff;
+        let v_adj = f32::from_bits((v | 0x3e80_0000) & 0x3eff_ffff);
+        let pow = 0.059914046f32;
+        let pow = pow.mul_add(v_adj, -0.10889456);
+        let pow = pow.mul_add(v_adj, 0.107963754);
+        let pow = pow.mul_add(v_adj, 0.018092343);
+
+        let idx = ((v >> 23) - 118) as usize;
+        let mul = 0x4000_0000 | (u32::from(POWTABLE_UPPER.0[idx]) << 18) | (u32::from(POWTABLE_LOWER.0[idx]) << 10);
+
+        let v = f32::from_bits(v);
+        let small = v * 12.92;
+        let acc = pow.mul_add(f32::from_bits(mul), -0.055);
+
+        *s = if v <= 0.0031308 { small } else { acc }.copysign(*s);
     }
 }
 
