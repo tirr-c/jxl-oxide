18e817a1631ef810b6ff7af166b3b28abe50bba5
Wonwoo Choi <chwo9843@gmail.com>
2023-09-26 01:15:54 +0900

    jxl-render: Simplify gaborish

--- crates/jxl-render/src/filter/impls.rs
+++ crates/jxl-render/src/filter/impls.rs
@@ -15,12 +15,15 @@ fn run_gabor_inner(fb: &mut jxl_grid::SimpleGrid<f32>, weight1: f32, weight2: f3
 
     let width = fb.width();
     let height = fb.height();
-    let input = fb.clone();
+    let mut input = vec![0f32; width * (height + 2)];
+    input[width..][..width * height].copy_from_slice(fb.buf());
+    input[..width].copy_from_slice(&fb.buf()[..width]);
+    input[width * (height + 1)..][..width].copy_from_slice(&fb.buf()[width * (height - 1)..][..width]);
 
-    let input = input.buf();
+    let input = &*input;
     let output = fb.buf_mut();
 
-    let len = width * (height - 2) - 2;
+    let len = width * height - 2;
     let center = &input[width + 1..][..len];
     let sides = [
         &input[1..][..len],
@@ -35,30 +38,6 @@ fn run_gabor_inner(fb: &mut jxl_grid::SimpleGrid<f32>, weight1: f32, weight2: f3
         &input[width * 2 + 2..][..len],
     ];
 
-    for (idx, out) in output[width + 1..][..len].iter_mut().enumerate() {
-        *out = (
-            center[idx] +
-            (sides[0][idx] + sides[1][idx] + sides[2][idx] + sides[3][idx]) * weight1 +
-            (diags[0][idx] + diags[1][idx] + diags[2][idx] + diags[3][idx]) * weight2
-        ) * global_weight;
-    }
-
-    // top side
-    let len = width - 2;
-    let center = &input[1..][..len];
-    let sides = [
-        &input[1..][..len],
-        &input[..len],
-        &input[2..][..len],
-        &input[width + 1..][..len],
-    ];
-    let diags = [
-        &input[..len],
-        &input[2..][..len],
-        &input[width..][..len],
-        &input[width + 2..][..len],
-    ];
-
     for (idx, out) in output[1..][..len].iter_mut().enumerate() {
         *out = (
             center[idx] +
@@ -67,33 +46,7 @@ fn run_gabor_inner(fb: &mut jxl_grid::SimpleGrid<f32>, weight1: f32, weight2: f3
         ) * global_weight;
     }
 
-    // bottom side
-    let len = width - 2;
-    let base = width * (height - 1);
-    let center = &input[base + 1..][..len];
-    let sides = [
-        &input[base - width + 1..][..len],
-        &input[base..][..len],
-        &input[base + 2..][..len],
-        &input[base + 1..][..len],
-    ];
-    let diags = [
-        &input[base - width..][..len],
-        &input[base - width + 2..][..len],
-        &input[base..][..len],
-        &input[base + 2..][..len],
-    ];
-
-    for (idx, out) in output[base + 1..][..len].iter_mut().enumerate() {
-        *out = (
-            center[idx] +
-            (sides[0][idx] + sides[1][idx] + sides[2][idx] + sides[3][idx]) * weight1 +
-            (diags[0][idx] + diags[1][idx] + diags[2][idx] + diags[3][idx]) * weight2
-        ) * global_weight;
-    }
-
     // left side
-    let len = height - 2;
     let center = &input[width..];
     let sides = [
         input,
@@ -107,16 +60,16 @@ fn run_gabor_inner(fb: &mut jxl_grid::SimpleGrid<f32>, weight1: f32, weight2: f3
         &input[width * 2..],
         &input[width * 2 + 1..],
     ];
-    for idx in 0..len {
-        output[width + idx * width] = (
-            center[idx * width] +
-            (sides[0][idx * width] + sides[1][idx * width] + sides[2][idx * width] + sides[3][idx * width]) * weight1 +
-            (diags[0][idx * width] + diags[1][idx * width] + diags[2][idx * width] + diags[3][idx * width]) * weight2
+    for idx in 0..height {
+        let offset = idx * width;
+        output[offset] = (
+            center[offset] +
+            (sides[0][offset] + sides[1][offset] + sides[2][offset] + sides[3][offset]) * weight1 +
+            (diags[0][offset] + diags[1][offset] + diags[2][offset] + diags[3][offset]) * weight2
         ) * global_weight;
     }
 
     // right side
-    let len = height - 2;
     let center = &input[width * 2 - 1..];
     let sides = [
         &input[width - 1..],
@@ -130,34 +83,12 @@ fn run_gabor_inner(fb: &mut jxl_grid::SimpleGrid<f32>, weight1: f32, weight2: f3
         &input[width * 3 - 2..],
         &input[width * 3 - 1..],
     ];
-    for idx in 0..len {
-        output[width * 2 - 1 + idx * width] = (
-            center[idx * width] +
-            (sides[0][idx * width] + sides[1][idx * width] + sides[2][idx * width] + sides[3][idx * width]) * weight1 +
-            (diags[0][idx * width] + diags[1][idx * width] + diags[2][idx * width] + diags[3][idx * width]) * weight2
+    for idx in 0..height {
+        let offset = idx * width;
+        output[width - 1 + offset] = (
+            center[offset] +
+            (sides[0][offset] + sides[1][offset] + sides[2][offset] + sides[3][offset]) * weight1 +
+            (diags[0][offset] + diags[1][offset] + diags[2][offset] + diags[3][offset]) * weight2
         ) * global_weight;
     }
-
-    // corners
-    output[0] = (
-        input[0] +
-        (input[0] + input[0] + input[1] + input[width]) * weight1 +
-        (input[0] + input[1] + input[width] + input[width + 1]) * weight2
-    ) * global_weight;
-    output[width - 1] = (
-        input[width - 1] +
-        (input[width - 1] + input[width - 1] + input[width - 2] + input[width * 2 - 1]) * weight1 +
-        (input[width - 1] + input[width - 2] + input[width * 2 - 2] + input[width * 2 - 1]) * weight2
-    ) * global_weight;
-    output[base] = (
-        input[base] +
-        (input[base] + input[base] + input[base - width] + input[base + 1]) * weight1 +
-        (input[base] + input[base - width] + input[base - width + 1] + input[base + 1]) * weight2
-    ) * global_weight;
-    let last = width * height - 1;
-    output[last] = (
-        input[last] +
-        (input[last] + input[last] + input[last - width] + input[last - 1]) * weight1 +
-        (input[last] + input[last - width] + input[last - width - 1] + input[last - 1]) * weight2
-    ) * global_weight;
 }
