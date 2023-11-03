a9e7674b2b0ea1645569ff92333aaf3c25b25a1b
Nereuxofficial <37740907+Nereuxofficial@users.noreply.github.com>
2023-11-03 14:31:29 +0100

    feat: Extended fuzzing harness, more crashes

--- crates/jxl-oxide/fuzz/fuzz_targets/decode.rs
+++ crates/jxl-oxide/fuzz/fuzz_targets/decode.rs
@@ -1,16 +1,32 @@
 use honggfuzz::fuzz;
 use jxl_oxide::JxlImage;
 
+fn decode(data: &[u8]) {
+    if let Ok(mut image) = JxlImage::from_reader(std::io::Cursor::new(data)) {
+        let header = image.image_header();
+        let max_size = u32::max(header.size.width, header.size.height);
+        drop(header);
+
+        // Skip huge images
+        if max_size > 65536 {
+            return;
+        }
+        for keyframe_idx in 0..image.num_loaded_keyframes() {
+            let _ = image.render_frame(keyframe_idx);
+        }
+    }
+}
+
 fn main() {
     loop {
         fuzz!(|data: &[u8]| {
-            let _ = JxlImage::from_reader(std::io::Cursor::new(data));
+            decode(data);
         });
     }
 }
 
 #[cfg(test)]
-mod tests{
+mod tests {
     use super::*;
     #[test]
     fn test_multiply_with_overflow_height() {
@@ -21,5 +37,22 @@ mod tests{
     fn test_crash_1() {
         let data = include_bytes!("../hfuzz_workspace/fuzz_decode/SIGABRT.PC.7ffff7d1383c.STACK.c9ec5b426.CODE.-6.ADDR.0.INSTR.mov____%eax,%ebx.fuzz");
         let _ = JxlImage::from_reader(std::io::Cursor::new(data));
+    }
+
+    #[test]
+    fn test_crash_2() {
+        let data = include_bytes!("../hfuzz_workspace/fuzz_decode/SIGABRT.PC.7ffff7d1383c.STACK.1b3d7db539.CODE.-6.ADDR.0.INSTR.mov____%eax,%ebx.fuzz");
+        decode(data);
+    }
+
+    #[test]
+    fn test_crash_3() {
+        let data = include_bytes!("../hfuzz_workspace/fuzz_decode/SIGABRT.PC.7ffff7d1383c.STACK.1be105aa8d.CODE.-6.ADDR.0.INSTR.mov____%eax,%ebx.fuzz");
+        decode(data);
+    }
+    #[test]
+    fn test_crash_out_of_bounds() {
+        let data = include_bytes!("../hfuzz_workspace/fuzz_decode/SIGABRT.PC.7ffff7d1383c.STACK.1be13466eb.CODE.-6.ADDR.0.INSTR.mov____%eax,%ebx.fuzz");
+        decode(data);
     }
 }
