e3a675dbb311f1d526156921523170f51794078e
Nereuxofficial <37740907+Nereuxofficial@users.noreply.github.com>
2023-11-03 15:22:36 +0100

    fix: Removed comments used for testing

--- crates/jxl-oxide-fuzz/fuzz_targets/decode.rs
+++ crates/jxl-oxide-fuzz/fuzz_targets/decode.rs
@@ -30,8 +30,8 @@ mod tests {
     use super::*;
     #[test]
     fn test_multiply_with_overflow_height() {
-        //let data = include_bytes!("../hfuzz_workspace/fuzz_decode/SIGABRT.PC.7ffff7d1383c.STACK.191f89ecd1.CODE.-6.ADDR.0.INSTR.mov____%eax,%ebx.jxl-oxide-fuzz");
-        //let _ = JxlImage::from_reader(std::io::Cursor::new(data));
+        let data = include_bytes!("../hfuzz_workspace/fuzz_decode/SIGABRT.PC.7ffff7d1383c.STACK.191f89ecd1.CODE.-6.ADDR.0.INSTR.mov____%eax,%ebx.jxl-oxide-fuzz");
+        let _ = JxlImage::from_reader(std::io::Cursor::new(data));
     }
     #[test]
     fn test_crash_1() {
