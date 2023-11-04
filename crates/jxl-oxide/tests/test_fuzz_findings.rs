c3a9fde4cee9d1347b3a11fc40714f79ce082842
Nereuxofficial <37740907+Nereuxofficial@users.noreply.github.com>
2023-11-04 10:29:06 +0100

    refactor: moved tests to own directory, added to .gitignore, committed Cargo.lock

--- crates/jxl-oxide/tests/test_fuzz_findings.rs
+++ crates/jxl-oxide/tests/test_fuzz_findings.rs
@@ -1,4 +1,5 @@
 use jxl_oxide::JxlImage;
+
 fn fuzz_decode(data: &[u8]) {
     if let Ok(mut image) = JxlImage::from_reader(std::io::Cursor::new(data)) {
         let header = image.image_header();
@@ -13,15 +14,23 @@ fn fuzz_decode(data: &[u8]) {
         }
     }
 }
-fn main() {
-    // Honggfuzz does not support windows yet
-    #[cfg(not(target_os = "windows"))]
-    {
-        use honggfuzz::fuzz;
-        loop {
-            fuzz!(|data: &[u8]| {
+
+// Macro to simplify writing these tests. To use it write the name of the file in the fuzz_findings directory without the .fuzz extension.
+macro_rules! test_by_include {
+    ($($name:ident),* $(,)?) => {
+        $(
+            #[ignore]
+            #[test]
+            fn $name() {
+                let data = include_bytes!(concat!("fuzz_findings/", stringify!($name), ".fuzz"));
                 fuzz_decode(data);
-            });
-        }
+            }
+        )*
     }
 }
+
+test_by_include!(
+    large_output_size,
+    multiply_integer_overflow,
+    out_of_bounds_access,
+);
