cc7d169fa63339def5e3b416447726d06c6a299e
Nereuxofficial <37740907+Nereuxofficial@users.noreply.github.com>
2023-11-04 11:58:51 +0100

    fix: max size not needed for tests

--- crates/jxl-oxide/tests/test_fuzz_findings.rs
+++ crates/jxl-oxide/tests/test_fuzz_findings.rs
@@ -4,11 +4,6 @@ fn fuzz_decode(data: &[u8]) {
     if let Ok(mut image) = JxlImage::from_reader(std::io::Cursor::new(data)) {
         let header = image.image_header();
         let max_size = u32::max(header.size.width, header.size.height);
-
-        // Skip huge images
-        if max_size > 65536 {
-            return;
-        }
         for keyframe_idx in 0..image.num_loaded_keyframes() {
             let _ = image.render_frame(keyframe_idx);
         }
