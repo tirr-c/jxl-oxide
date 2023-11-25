bde70170dc6f31bdf89f328b7b95a668f615f3d9
Wonwoo Choi <chwo9843@gmail.com>
2023-11-25 21:43:51 +0900

    Fix build in x86_64

--- crates/jxl-render/src/vardct/x86_64.rs
+++ crates/jxl-render/src/vardct/x86_64.rs
@@ -1,3 +1,5 @@
+use jxl_grid::AllocTracker;
+
 use super::generic;
 
 pub fn adaptive_lf_smoothing_impl(
@@ -5,13 +7,14 @@ pub fn adaptive_lf_smoothing_impl(
     height: usize,
     lf_image: [&mut [f32]; 3],
     lf_scale: [f32; 3],
-) {
+    tracker: Option<&AllocTracker>,
+) -> crate::Result<()> {
     if is_x86_feature_detected!("avx2") && is_x86_feature_detected!("fma") {
         // SAFETY: Feature set is checked above.
-        return unsafe { adaptive_lf_smoothing_core_avx2(width, height, lf_image, lf_scale) };
+        return unsafe { adaptive_lf_smoothing_core_avx2(width, height, lf_image, lf_scale, tracker) };
     }
 
-    generic::adaptive_lf_smoothing_impl(width, height, lf_image, lf_scale)
+    generic::adaptive_lf_smoothing_impl(width, height, lf_image, lf_scale, tracker)
 }
 
 #[target_feature(enable = "avx2")]
@@ -21,6 +24,7 @@ unsafe fn adaptive_lf_smoothing_core_avx2(
     height: usize,
     lf_image: [&mut [f32]; 3],
     lf_scale: [f32; 3],
-) {
-    generic::adaptive_lf_smoothing_impl(width, height, lf_image, lf_scale)
+    tracker: Option<&AllocTracker>,
+) -> crate::Result<()> {
+    generic::adaptive_lf_smoothing_impl(width, height, lf_image, lf_scale, tracker)
 }
