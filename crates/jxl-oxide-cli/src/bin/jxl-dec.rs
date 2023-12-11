12383b0f28b5783f89767a1d049dc9882d4b6862
Wonwoo Choi <chwo9843@gmail.com>
2023-12-12 00:26:25 +0900

    jxl-oxide-cli: Add `target_colorspace`

--- crates/jxl-oxide-cli/src/bin/jxl-dec.rs
+++ crates/jxl-oxide-cli/src/bin/jxl-dec.rs
@@ -3,7 +3,8 @@ use std::{io::prelude::*, path::PathBuf};
 use clap::Parser;
 use jxl_oxide::{
     color::{
-        ColourSpace, EnumColourEncoding, Primaries, RenderingIntent, TransferFunction, WhitePoint,
+        ColourSpace, Customxy, EnumColourEncoding, Primaries, RenderingIntent, TransferFunction,
+        WhitePoint,
     },
     AllocTracker, CropInfo, FrameBuffer, JxlImage, JxlThreadPool, PixelFormat, Render,
 };
@@ -100,8 +101,29 @@ struct Args {
     #[arg(value_enum, short = 'f', long, default_value_t = OutputFormat::Png)]
     output_format: OutputFormat,
     /// (unstable) Target colorspace specification
-    #[arg(long)]
-    target_colorspace: Option<ColorspaceSpec>,
+    ///
+    /// Specification string consists of (optional) preset and a sequence of parameters delimited by semicolons.
+    ///
+    /// Parameters have a syntax of `name=value`. Valid parameter names are:
+    /// - `type`: Color space type.
+    ///   Valid values: `rgb`, `gray`, `xyb`.
+    /// - `gamut`: Color gamut. Invalid if type is Gray or XYB.
+    ///   Valid values: `srgb`, `p3`, `bt2100`, three xy-chromaticity coordinates delimited by commas.
+    /// - `white_point`: White point. Invalid if type is XYB.
+    ///   Valid values: `d65`, `dci`, `e`, xy-chromaticity coordinate.
+    /// - `tf`: Transfer function. Invalid if type is XYB.
+    ///   Valid values: `srgb`, `bt709`, `dci`, `pq`, `hlg`, `linear`, gamma value.
+    /// - `intent`: Rendering intent.
+    ///   Valid values: `relative`, `perceptual`, `saturation`, `absolute`.
+    ///
+    /// Presets define a set of parameters commonly used together:
+    /// - `srgb`: type=rgb;gamut=srgb;white_point=d65;tf=srgb;intent=relative
+    /// - `display_p3`: type=rgb;gamut=p3;white_point=d65;tf=srgb;intent=relative
+    /// - `rec2020`: type=rgb;gamut=bt2100;white_point=d65;tf=bt709;intent=relative
+    /// - `rec2100`: type=rgb;gamut=bt2100;white_point=d65;intent=relative
+    ///   Transfer function is not set for `rec2100` preset; one should be provided, e.g. `rec2100;tf=pq`
+    #[arg(long, value_parser = parse_color_encoding, verbatim_doc_comment)]
+    target_colorspace: Option<EnumColourEncoding>,
     /// (unstable) Path to target ICC profile
     #[arg(long)]
     target_icc: Option<PathBuf>,
@@ -136,51 +158,27 @@ struct ColorspaceSpec {
 }
 
 #[derive(Debug)]
-enum ColorspaceSpecParseErrorKind {
-    Empty,
-    UnknownPreset(String),
-    UnknownParam(String),
-    InvalidParam(&'static str),
-    InvalidParamValue { name: String, value: String },
-    MissingParams(Vec<&'static str>),
+struct ColorspaceSpecParseError(std::borrow::Cow<'static, str>);
+
+impl From<&'static str> for ColorspaceSpecParseError {
+    fn from(value: &'static str) -> Self {
+        Self(value.into())
+    }
 }
 
-#[derive(Debug)]
-struct ColorspaceSpecParseError {
-    kind: ColorspaceSpecParseErrorKind,
-    message: String,
+impl From<String> for ColorspaceSpecParseError {
+    fn from(value: String) -> Self {
+        Self(value.into())
+    }
 }
 
 impl std::fmt::Display for ColorspaceSpecParseError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.message)
+        write!(f, "{}", self.0)
     }
 }
 
 impl std::error::Error for ColorspaceSpecParseError {}
-
-impl std::str::FromStr for ColorspaceSpec {
-    type Err = ColorspaceSpecParseError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut params = s.split(';').peekable();
-
-        let first = params.peek().ok_or(ColorspaceSpecParseError {
-            message: String::from("parameters are required"),
-            kind: ColorspaceSpecParseErrorKind::Empty,
-        })?;
-        let mut spec = ColorspaceSpec::from_preset(first).or_else(|_| {
-            let mut spec = ColorspaceSpec::new();
-            spec.add_param(first).map(|_| spec)
-        })?;
-
-        for param in params {
-            spec.add_param(param)?;
-        }
-
-        Ok(spec)
-    }
-}
 
 impl ColorspaceSpec {
     fn new() -> Self {
@@ -188,7 +186,8 @@ impl ColorspaceSpec {
     }
 
     fn from_preset(preset: &str) -> Result<Self, ColorspaceSpecParseError> {
-        Ok(match preset {
+        let preset_lowercase = preset.to_lowercase();
+        Ok(match &*preset_lowercase {
             "srgb" => ColorspaceSpec {
                 ty: Some(ColourSpace::Rgb),
                 white_point: Some(WhitePoint::D65),
@@ -203,14 +202,14 @@ impl ColorspaceSpec {
                 tf: Some(TransferFunction::Srgb),
                 intent: Some(RenderingIntent::Relative),
             },
-            "bt2020" => ColorspaceSpec {
+            "rec2020" | "rec.2020" => ColorspaceSpec {
                 ty: Some(ColourSpace::Rgb),
                 white_point: Some(WhitePoint::D65),
                 gamut: Some(Primaries::Bt2100),
                 tf: Some(TransferFunction::Bt709),
                 intent: Some(RenderingIntent::Relative),
             },
-            "bt2100" => ColorspaceSpec {
+            "rec2100" | "rec.2100" => ColorspaceSpec {
                 ty: Some(ColourSpace::Rgb),
                 white_point: Some(WhitePoint::D65),
                 gamut: Some(Primaries::Bt2100),
@@ -218,17 +217,223 @@ impl ColorspaceSpec {
                 intent: Some(RenderingIntent::Relative),
             },
             _ => {
-                return Err(ColorspaceSpecParseError {
-                    kind: ColorspaceSpecParseErrorKind::UnknownParam(preset.to_owned()),
-                    message: format!("unknown preset: {preset}"),
-                })
+                return Err(format!("unknown preset `{preset}`").into());
             }
         })
     }
 
     fn add_param(&mut self, param: &str) -> Result<(), ColorspaceSpecParseError> {
-        todo!()
+        let (name, value) = param
+            .split_once('=')
+            .ok_or_else(|| format!("`{param}` is not a parameter spec"))?;
+        let name_lowercase = name.to_ascii_lowercase();
+        let value_lowercase = value.to_ascii_lowercase();
+        match &*name_lowercase {
+            "type" | "color_space" => match &*value_lowercase {
+                "rgb" => self.ty = Some(ColourSpace::Rgb),
+                "xyb" => {
+                    let mut invalid_option = None;
+                    if self.white_point.is_some() {
+                        invalid_option = Some("white point");
+                    } else if self.gamut.is_some() {
+                        invalid_option = Some("color gamut");
+                    } else if self.tf.is_some() {
+                        invalid_option = Some("transfer function");
+                    }
+                    if let Some(invalid_option) = invalid_option {
+                        return Err(format!(
+                            "cannot set {invalid_option} when color space type is XYB"
+                        )
+                        .into());
+                    }
+
+                    self.ty = Some(ColourSpace::Xyb);
+                }
+                "gray" | "grey" | "grayscale" | "greyscale" => {
+                    if self.gamut.is_some() {
+                        return Err(
+                            "cannot set color gamut when color space type is Grayscale".into()
+                        );
+                    }
+
+                    self.ty = Some(ColourSpace::Grey);
+                }
+                _ => return Err(format!("unknown color space type `{value}`").into()),
+            },
+            "white_point" | "wp" => {
+                if let Some(ColourSpace::Xyb) = self.ty {
+                    return Err("cannot set white point if color space type is XYB".into());
+                }
+
+                let wp = match &*value_lowercase {
+                    "d65" => WhitePoint::D65,
+                    "dci" => WhitePoint::Dci,
+                    "e" => WhitePoint::E,
+                    customxy => {
+                        let (x, y) = customxy
+                            .split_once(',')
+                            .ok_or_else(|| format!("invalid white point `{value}`"))?;
+                        let x = x
+                            .parse::<f32>()
+                            .map_err(|_| format!("cannot parse `{x}` as float"))?;
+                        let y = y
+                            .parse::<f32>()
+                            .map_err(|_| format!("cannot parse `{y}` as float"))?;
+                        let wp = validate_customxy(x, y)?;
+                        WhitePoint::Custom(wp)
+                    }
+                };
+                self.white_point = Some(wp);
+            }
+            "gamut" | "primaries" => {
+                if let Some(ColourSpace::Xyb) = self.ty {
+                    return Err("cannot set white point if color space type is XYB".into());
+                }
+                if let Some(ColourSpace::Grey) = self.ty {
+                    return Err("cannot set white point if color space type is Grayscale".into());
+                }
+
+                let gamut = match &*value_lowercase {
+                    "srgb" | "bt709" => Primaries::Srgb,
+                    "p3" => Primaries::P3,
+                    "2020" | "bt2020" | "bt.2020" | "rec2020" | "rec.2020" | "2100" | "bt2100"
+                    | "bt.2100" | "rec2100" | "rec.2100" => Primaries::Bt2100,
+                    customxy => {
+                        let mut values = customxy.split(',').map(|x| {
+                            x.parse::<f32>()
+                                .map_err(|_| format!("cannot parse `{x}` as float"))
+                        });
+                        let (Some(rx), Some(ry), Some(gx), Some(gy), Some(bx), Some(by), None) = (
+                            values.next(),
+                            values.next(),
+                            values.next(),
+                            values.next(),
+                            values.next(),
+                            values.next(),
+                            values.next(),
+                        ) else {
+                            return Err(format!("invalid gamut `{value}`").into());
+                        };
+
+                        let red = validate_customxy(rx?, ry?)?;
+                        let green = validate_customxy(gx?, gy?)?;
+                        let blue = validate_customxy(bx?, by?)?;
+                        Primaries::Custom { red, green, blue }
+                    }
+                };
+                self.gamut = Some(gamut);
+            }
+            "tf" | "transfer_function" | "curve" | "tone_curve" => {
+                if let Some(ColourSpace::Xyb) = self.ty {
+                    return Err("cannot set transfer function if color space type is XYB".into());
+                }
+
+                let tf = match &*value_lowercase {
+                    "srgb" => TransferFunction::Srgb,
+                    "bt709" | "bt.709" | "709" => TransferFunction::Bt709,
+                    "dci" => TransferFunction::Dci,
+                    "pq" | "perceptual_quantizer" => TransferFunction::Pq,
+                    "hlg" | "hybrid_log_gamma" => TransferFunction::Hlg,
+                    "linear" => TransferFunction::Linear,
+                    gamma => {
+                        let gamma = gamma
+                            .parse::<f32>()
+                            .map_err(|_| format!("invalid transfer function `{value}`"))?;
+                        if !gamma.is_finite() || gamma < 1f32 {
+                            return Err(format!("gamma of {gamma} is invalid").into());
+                        }
+
+                        TransferFunction::Gamma((1e7 / gamma + 0.5) as u32)
+                    }
+                };
+                self.tf = Some(tf);
+            }
+            "intent" | "rendering_intent" => {
+                let intent = match &*value_lowercase {
+                    "relative" | "rel" | "relative_colorimetric" => RenderingIntent::Relative,
+                    "perceptual" | "per" => RenderingIntent::Perceptual,
+                    "saturation" | "sat" => RenderingIntent::Saturation,
+                    "absolute" | "abs" | "absolute_colorimetric" => RenderingIntent::Absolute,
+                    _ => return Err(format!("invalid rendering intent `{value}`").into()),
+                };
+                self.intent = Some(intent);
+            }
+            _ => return Err(format!("invalid parameter `{name}`").into()),
+        }
+
+        Ok(())
     }
+}
+
+fn validate_customxy(x: f32, y: f32) -> Result<Customxy, ColorspaceSpecParseError> {
+    if x.is_finite() && y.is_finite() {
+        Ok(Customxy {
+            x: (x * 1e6 + 0.5) as i32,
+            y: (y * 1e6 + 0.5) as i32,
+        })
+    } else {
+        Err(format!("xy-chromaticity coordinate ({x}, {y}) is invalid").into())
+    }
+}
+
+fn parse_color_encoding(val: &str) -> Result<EnumColourEncoding, ColorspaceSpecParseError> {
+    let mut params = val.split(';');
+
+    let first = params.next().ok_or("parameters are required")?;
+    let mut spec = ColorspaceSpec::from_preset(first).or_else(|preset_err| {
+        let mut spec = ColorspaceSpec::new();
+        spec.add_param(first).map(|_| spec).map_err(|param_err| {
+            if first.contains('=') {
+                param_err
+            } else {
+                preset_err
+            }
+        })
+    })?;
+
+    for param in params {
+        spec.add_param(param)?;
+    }
+
+    let ty = spec.ty.ok_or("color space type is required")?;
+    Ok(match ty {
+        ColourSpace::Rgb => {
+            let white_point = spec.white_point.ok_or("white point is required")?;
+            let primaries = spec.gamut.ok_or("color gamut is required")?;
+            let tf = spec.tf.ok_or("transfer function is required")?;
+            let rendering_intent = spec.intent.ok_or("rendering intent is required")?;
+            EnumColourEncoding {
+                colour_space: ColourSpace::Rgb,
+                white_point,
+                primaries,
+                tf,
+                rendering_intent,
+            }
+        }
+        ColourSpace::Grey => {
+            let white_point = spec.white_point.ok_or("white point is required")?;
+            let tf = spec.tf.ok_or("transfer function is required")?;
+            let rendering_intent = spec.intent.ok_or("rendering intent is required")?;
+            EnumColourEncoding {
+                colour_space: ColourSpace::Grey,
+                white_point,
+                primaries: Primaries::Srgb,
+                tf,
+                rendering_intent,
+            }
+        }
+        ColourSpace::Xyb => {
+            let rendering_intent = spec.intent.ok_or("rendering intent is required")?;
+            EnumColourEncoding {
+                colour_space: ColourSpace::Xyb,
+                white_point: WhitePoint::D65,
+                primaries: Primaries::Srgb,
+                tf: TransferFunction::Srgb,
+                rendering_intent,
+            }
+        }
+        _ => unreachable!(),
+    })
 }
 
 fn parse_crop_info(s: &str) -> Result<CropInfo, std::num::ParseIntError> {
@@ -319,6 +524,9 @@ fn main() {
         tracing::debug!("Setting target ICC profile");
         let icc_profile = std::fs::read(icc_path).expect("Failed to read ICC profile");
         image.request_icc(icc_profile);
+    } else if let Some(encoding) = args.target_colorspace {
+        tracing::debug!(?encoding, "Setting target color space");
+        image.request_color_encoding(encoding);
     } else if output_png && image.pixel_format().has_black() {
         tracing::debug!("Input is CMYK; setting target color encoding to sRGB");
         image.request_color_encoding(EnumColourEncoding::srgb(
