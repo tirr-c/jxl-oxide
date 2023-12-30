8be47bcdb75b121f76341f142da621185d5bfa3f
Wonwoo Choi <chwo9843@gmail.com>
2023-12-30 16:18:50 +0900

    jxl-color: x86_64 SIMD gamut mapper

--- crates/jxl-color/src/convert/gamut_map.rs
+++ crates/jxl-color/src/convert/gamut_map.rs
@@ -17,11 +17,41 @@ pub(super) fn gamut_map(
     if is_x86_feature_detected!("fma") && is_x86_feature_detected!("sse4.1") {
         if is_x86_feature_detected!("avx2") {
             // AVX2
+            // SAFETY: features are checked above.
+            unsafe {
+                (r, g, b) = gamut_map_x86_64_avx2(r, g, b, luminances, saturation_factor);
+            }
         }
 
         // SSE4.1 + FMA
+        // SAFETY: features are checked above.
+        unsafe {
+            (r, g, b) = gamut_map_x86_64_fma(r, g, b, luminances, saturation_factor);
+        }
     } else {
-        // SSE2
+        // SAFETY: x86_64 implies SSE2.
+        unsafe {
+            let mut r_it = r.chunks_exact_mut(4);
+            let mut g_it = g.chunks_exact_mut(4);
+            let mut b_it = b.chunks_exact_mut(4);
+
+            for ((r, g), b) in (&mut r_it).zip(&mut g_it).zip(&mut b_it) {
+                let rgb = [
+                    std::arch::x86_64::_mm_loadu_ps(r.as_ptr()),
+                    std::arch::x86_64::_mm_loadu_ps(g.as_ptr()),
+                    std::arch::x86_64::_mm_loadu_ps(b.as_ptr()),
+                ];
+                let [vr, vg, vb] =
+                    crate::gamut::map_gamut_x86_64_sse2(rgb, luminances, saturation_factor);
+                std::arch::x86_64::_mm_storeu_ps(r.as_mut_ptr(), vr);
+                std::arch::x86_64::_mm_storeu_ps(g.as_mut_ptr(), vg);
+                std::arch::x86_64::_mm_storeu_ps(b.as_mut_ptr(), vb);
+            }
+
+            r = r_it.into_remainder();
+            g = g_it.into_remainder();
+            b = b_it.into_remainder();
+        }
     }
 
     #[cfg(target_arch = "aarch64")]
@@ -59,4 +89,71 @@ pub(super) fn gamut_map(
         *g = mapped[1];
         *b = mapped[2];
     }
+}
+
+#[cfg(target_arch = "x86_64")]
+#[target_feature(enable = "avx2")]
+#[target_feature(enable = "fma")]
+#[target_feature(enable = "sse4.1")]
+pub(super) unsafe fn gamut_map_x86_64_avx2<'r, 'g, 'b>(
+    r: &'r mut [f32],
+    g: &'g mut [f32],
+    b: &'b mut [f32],
+    luminances: [f32; 3],
+    saturation_factor: f32,
+) -> (&'r mut [f32], &'g mut [f32], &'b mut [f32]) {
+    let mut r_it = r.chunks_exact_mut(8);
+    let mut g_it = g.chunks_exact_mut(8);
+    let mut b_it = b.chunks_exact_mut(8);
+
+    for ((r, g), b) in (&mut r_it).zip(&mut g_it).zip(&mut b_it) {
+        let rgb = [
+            std::arch::x86_64::_mm256_loadu_ps(r.as_ptr()),
+            std::arch::x86_64::_mm256_loadu_ps(g.as_ptr()),
+            std::arch::x86_64::_mm256_loadu_ps(b.as_ptr()),
+        ];
+        let [vr, vg, vb] = crate::gamut::map_gamut_x86_64_avx2(rgb, luminances, saturation_factor);
+        std::arch::x86_64::_mm256_storeu_ps(r.as_mut_ptr(), vr);
+        std::arch::x86_64::_mm256_storeu_ps(g.as_mut_ptr(), vg);
+        std::arch::x86_64::_mm256_storeu_ps(b.as_mut_ptr(), vb);
+    }
+
+    (
+        r_it.into_remainder(),
+        g_it.into_remainder(),
+        b_it.into_remainder(),
+    )
+}
+
+#[cfg(target_arch = "x86_64")]
+#[target_feature(enable = "fma")]
+#[target_feature(enable = "sse4.1")]
+pub(super) unsafe fn gamut_map_x86_64_fma<'r, 'g, 'b>(
+    r: &'r mut [f32],
+    g: &'g mut [f32],
+    b: &'b mut [f32],
+    luminances: [f32; 3],
+    saturation_factor: f32,
+) -> (&'r mut [f32], &'g mut [f32], &'b mut [f32]) {
+    let mut r_it = r.chunks_exact_mut(4);
+    let mut g_it = g.chunks_exact_mut(4);
+    let mut b_it = b.chunks_exact_mut(4);
+
+    for ((r, g), b) in (&mut r_it).zip(&mut g_it).zip(&mut b_it) {
+        let rgb = [
+            std::arch::x86_64::_mm_loadu_ps(r.as_ptr()),
+            std::arch::x86_64::_mm_loadu_ps(g.as_ptr()),
+            std::arch::x86_64::_mm_loadu_ps(b.as_ptr()),
+        ];
+        let [vr, vg, vb] = crate::gamut::map_gamut_x86_64_fma(rgb, luminances, saturation_factor);
+        std::arch::x86_64::_mm_storeu_ps(r.as_mut_ptr(), vr);
+        std::arch::x86_64::_mm_storeu_ps(g.as_mut_ptr(), vg);
+        std::arch::x86_64::_mm_storeu_ps(b.as_mut_ptr(), vb);
+    }
+
+    (
+        r_it.into_remainder(),
+        g_it.into_remainder(),
+        b_it.into_remainder(),
+    )
 }
--- crates/jxl-color/src/gamut.rs
+++ crates/jxl-color/src/gamut.rs
@@ -40,6 +40,180 @@ pub(crate) fn map_gamut_generic(
     mixed_rgb.map(|v| v / max_color_val)
 }
 
+#[cfg(target_arch = "x86_64")]
+#[target_feature(enable = "avx2")]
+#[target_feature(enable = "fma")]
+#[target_feature(enable = "sse4.1")]
+#[inline]
+pub(crate) unsafe fn map_gamut_x86_64_avx2(
+    rgb: [std::arch::x86_64::__m256; 3],
+    luminance: [f32; 3],
+    saturation_factor: f32,
+) -> [std::arch::x86_64::__m256; 3] {
+    use jxl_grid::SimdVector;
+    use std::arch::x86_64::*;
+
+    let [vr, vg, vb] = rgb;
+    let [vlr, vlg, vlb] = luminance.map(|v| _mm256_set1_ps(v));
+    let y = _mm256_fmadd_ps(vb, vlb, _mm256_fmadd_ps(vg, vlg, vr.mul(vlr)));
+
+    let (gray_saturation, gray_luminance) = rgb.into_iter().fold(
+        (_mm256_setzero_ps(), _mm256_setzero_ps()),
+        |(gray_saturation, gray_luminance), v| {
+            let v_sub_y = v.sub(y);
+            let inv_v_sub_y = _mm256_set1_ps(1.0).div(_mm256_blendv_ps(
+                v_sub_y,
+                _mm256_set1_ps(1.0),
+                _mm256_cmp_ps::<_CMP_EQ_OQ>(v_sub_y, _mm256_setzero_ps()),
+            ));
+            let v_over_v_sub_y = v.mul(inv_v_sub_y);
+
+            let gray_saturation = _mm256_blendv_ps(
+                _mm256_max_ps(gray_saturation, v_over_v_sub_y),
+                gray_saturation,
+                _mm256_cmp_ps::<_CMP_GE_OQ>(v_sub_y, _mm256_setzero_ps()),
+            );
+            let gray_luminance = _mm256_max_ps(
+                _mm256_blendv_ps(
+                    v_over_v_sub_y.sub(inv_v_sub_y),
+                    gray_saturation,
+                    _mm256_cmp_ps::<_CMP_LE_OQ>(v_sub_y, _mm256_setzero_ps()),
+                ),
+                gray_luminance,
+            );
+
+            (gray_saturation, gray_luminance)
+        },
+    );
+
+    let gray_mix = gray_saturation
+        .sub(gray_luminance)
+        .muladd(_mm256_set1_ps(saturation_factor), gray_luminance);
+    let gray_mix = _mm256_max_ps(
+        _mm256_setzero_ps(),
+        _mm256_min_ps(_mm256_set1_ps(1.0), gray_mix),
+    );
+
+    let mixed_rgb = rgb.map(|v| gray_mix.muladd(y.sub(v), v));
+    let max_color_val = rgb
+        .into_iter()
+        .fold(_mm256_set1_ps(1.0), |acc, v| _mm256_max_ps(acc, v));
+    mixed_rgb.map(|v| v.div(max_color_val))
+}
+
+#[cfg(target_arch = "x86_64")]
+#[target_feature(enable = "fma")]
+#[target_feature(enable = "sse4.1")]
+#[inline]
+pub(crate) unsafe fn map_gamut_x86_64_fma(
+    rgb: [std::arch::x86_64::__m128; 3],
+    luminance: [f32; 3],
+    saturation_factor: f32,
+) -> [std::arch::x86_64::__m128; 3] {
+    use jxl_grid::SimdVector;
+    use std::arch::x86_64::*;
+
+    let [vr, vg, vb] = rgb;
+    let [vlr, vlg, vlb] = luminance.map(|v| _mm_set1_ps(v));
+    let y = _mm_fmadd_ps(vb, vlb, _mm_fmadd_ps(vg, vlg, vr.mul(vlr)));
+
+    let (gray_saturation, gray_luminance) = rgb.into_iter().fold(
+        (_mm_setzero_ps(), _mm_setzero_ps()),
+        |(gray_saturation, gray_luminance), v| {
+            let v_sub_y = v.sub(y);
+            let inv_v_sub_y = _mm_set1_ps(1.0).div(_mm_blendv_ps(
+                v_sub_y,
+                _mm_set1_ps(1.0),
+                _mm_cmpeq_ps(v_sub_y, _mm_setzero_ps()),
+            ));
+            let v_over_v_sub_y = v.mul(inv_v_sub_y);
+
+            let gray_saturation = _mm_blendv_ps(
+                _mm_max_ps(gray_saturation, v_over_v_sub_y),
+                gray_saturation,
+                _mm_cmpge_ps(v_sub_y, _mm_setzero_ps()),
+            );
+            let gray_luminance = _mm_max_ps(
+                _mm_blendv_ps(
+                    v_over_v_sub_y.sub(inv_v_sub_y),
+                    gray_saturation,
+                    _mm_cmple_ps(v_sub_y, _mm_setzero_ps()),
+                ),
+                gray_luminance,
+            );
+
+            (gray_saturation, gray_luminance)
+        },
+    );
+
+    let gray_mix = gray_saturation
+        .sub(gray_luminance)
+        .muladd(_mm_set1_ps(saturation_factor), gray_luminance);
+    let gray_mix = _mm_max_ps(_mm_setzero_ps(), _mm_min_ps(_mm_set1_ps(1.0), gray_mix));
+
+    let mixed_rgb = rgb.map(|v| gray_mix.muladd(y.sub(v), v));
+    let max_color_val = rgb
+        .into_iter()
+        .fold(_mm_set1_ps(1.0), |acc, v| _mm_max_ps(acc, v));
+    mixed_rgb.map(|v| v.div(max_color_val))
+}
+
+#[cfg(target_arch = "x86_64")]
+#[inline]
+pub(crate) unsafe fn map_gamut_x86_64_sse2(
+    rgb: [std::arch::x86_64::__m128; 3],
+    luminance: [f32; 3],
+    saturation_factor: f32,
+) -> [std::arch::x86_64::__m128; 3] {
+    use jxl_grid::SimdVector;
+    use std::arch::x86_64::*;
+
+    let [vr, vg, vb] = rgb;
+    let [vlr, vlg, vlb] = luminance.map(|v| _mm_set1_ps(v));
+    let y = vb.muladd(vlb, vg.muladd(vlg, vr.mul(vlr)));
+
+    let (gray_saturation, gray_luminance) = rgb.into_iter().fold(
+        (_mm_setzero_ps(), _mm_setzero_ps()),
+        |(gray_saturation, gray_luminance), v| {
+            let v_sub_y = v.sub(y);
+            let mask = _mm_cmpeq_ps(v_sub_y, _mm_setzero_ps());
+            let inv_v_sub_y = _mm_set1_ps(1.0).div(_mm_or_ps(
+                _mm_andnot_ps(mask, v_sub_y),
+                _mm_and_ps(mask, _mm_set1_ps(1.0)),
+            ));
+            let v_over_v_sub_y = v.mul(inv_v_sub_y);
+
+            let mask = _mm_cmpge_ps(v_sub_y, _mm_setzero_ps());
+            let gray_saturation = _mm_or_ps(
+                _mm_andnot_ps(mask, _mm_max_ps(gray_saturation, v_over_v_sub_y)),
+                _mm_and_ps(mask, gray_saturation),
+            );
+
+            let mask = _mm_cmple_ps(v_sub_y, _mm_setzero_ps());
+            let gray_luminance = _mm_max_ps(
+                _mm_or_ps(
+                    _mm_andnot_ps(mask, v_over_v_sub_y.sub(inv_v_sub_y)),
+                    _mm_and_ps(mask, gray_saturation),
+                ),
+                gray_luminance,
+            );
+
+            (gray_saturation, gray_luminance)
+        },
+    );
+
+    let gray_mix = gray_saturation
+        .sub(gray_luminance)
+        .muladd(_mm_set1_ps(saturation_factor), gray_luminance);
+    let gray_mix = _mm_max_ps(_mm_setzero_ps(), _mm_min_ps(_mm_set1_ps(1.0), gray_mix));
+
+    let mixed_rgb = rgb.map(|v| gray_mix.muladd(y.sub(v), v));
+    let max_color_val = rgb
+        .into_iter()
+        .fold(_mm_set1_ps(1.0), |acc, v| _mm_max_ps(acc, v));
+    mixed_rgb.map(|v| v.div(max_color_val))
+}
+
 #[cfg(target_arch = "aarch64")]
 #[target_feature(enable = "neon")]
 #[inline]
