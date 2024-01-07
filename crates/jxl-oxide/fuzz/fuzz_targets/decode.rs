f34f2b662147d139bec8885976f5970e1125a719
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2024-01-07 11:31:52 +0000

    Add an initial fuzzing harness using cargo-fuzz (#172)

---
+++ crates/jxl-oxide/fuzz/fuzz_targets/decode.rs
@@ -0,0 +1,28 @@
+#![no_main]
+
+use libfuzzer_sys::fuzz_target;
+
+fuzz_target!(|data: &[u8]| {
+    fuzz_decode(data);
+});
+
+use jxl_oxide::{AllocTracker, JxlImage, JxlThreadPool};
+
+fn fuzz_decode(data: &[u8]) {
+    let image = JxlImage::builder()
+        .pool(JxlThreadPool::none())
+        .alloc_tracker(AllocTracker::with_limit(128 * 1024 * 1024)) // 128 MiB
+        .read(std::io::Cursor::new(data));
+    if let Ok(image) = image {
+        let header = image.image_header();
+        let max_size = u32::max(header.size.width, header.size.height);
+
+        // Skip huge images
+        if max_size > 65536 {
+            return;
+        }
+        for keyframe_idx in 0..image.num_loaded_keyframes() {
+            let _ = image.render_frame(keyframe_idx);
+        }
+    }
+}
