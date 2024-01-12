4d4eac23de4fcf3764417e6022a5f92e7dafeb96
Wonwoo Choi <chwo9843@gmail.com>
2024-01-12 13:05:19 +0900

    jxl-oxide-cli: Log error information instead of panicking

--- crates/jxl-oxide-cli/src/lib.rs
+++ crates/jxl-oxide-cli/src/lib.rs
@@ -1,7 +1,11 @@
 pub mod commands;
 pub mod decode;
+pub mod error;
 #[cfg(feature = "__devtools")]
 pub mod generate_fixture;
 pub mod info;
 
 pub use commands::{Args, Subcommands};
+pub use error::Error;
+
+type Result<T> = std::result::Result<T, Error>;
