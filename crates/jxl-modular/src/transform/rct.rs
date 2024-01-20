d5a5e0a1eb9aff5dc8bbe9a9027056b0b95a4377
Wonwoo Choi <chwo9843@gmail.com>
2024-01-20 14:44:38 +0900

    jxl-modular: Check grid step in RCT

--- crates/jxl-modular/src/transform/rct.rs
+++ crates/jxl-modular/src/transform/rct.rs
@@ -6,6 +6,11 @@ use crate::Sample;
 
 pub fn inverse_rct<S: Sample>(permutation: u32, ty: u32, grids: [&mut CutGrid<S>; 3]) {
     let [a, b, c] = grids;
+    if a.step() != 0 || b.step() != 0 || c.step() != 0 {
+        do_slow(permutation, ty, [a, b, c]);
+        return;
+    }
+
     if let (Some(a), Some(b), Some(c)) = (
         S::try_as_i16_cut_grid_mut(a),
         S::try_as_i16_cut_grid_mut(b),
@@ -26,6 +31,10 @@ fn do_i16(permutation: u32, ty: u32, grids: [&mut CutGrid<i16>; 3]) {
     let permutation = permutation as usize;
 
     let [a, b, c] = grids;
+    assert_eq!(a.step(), 1);
+    assert_eq!(b.step(), 1);
+    assert_eq!(c.step(), 1);
+
     let width = a.width();
     let height = a.height();
     assert_eq!(width, b.width());
@@ -74,6 +83,10 @@ fn do_i32(permutation: u32, ty: u32, grids: [&mut CutGrid<i32>; 3]) {
     let permutation = permutation as usize;
 
     let [a, b, c] = grids;
+    assert_eq!(a.step(), 1);
+    assert_eq!(b.step(), 1);
+    assert_eq!(c.step(), 1);
+
     let width = a.width();
     let height = a.height();
     assert_eq!(width, b.width());
@@ -113,6 +126,50 @@ fn do_i32(permutation: u32, ty: u32, grids: [&mut CutGrid<i32>; 3]) {
             *samples[permutation % 3] = d.0;
             *samples[(permutation + 1 + (permutation / 3)) % 3] = e.0;
             *samples[(permutation + 2 - (permutation / 3)) % 3] = f.0;
+        }
+    }
+}
+
+fn do_slow<S: Sample>(permutation: u32, ty: u32, grids: [&mut CutGrid<S>; 3]) {
+    let permutation = permutation as usize;
+
+    let [a, b, c] = grids;
+    let width = a.width();
+    let height = a.height();
+    assert_eq!(width, b.width());
+    assert_eq!(width, c.width());
+    assert_eq!(height, b.height());
+    assert_eq!(height, c.height());
+
+    for y in 0..height {
+        for x in 0..width {
+            let samples = [a.get_mut(x, y), b.get_mut(x, y), c.get_mut(x, y)];
+
+            let a = Wrapping(samples[0].to_i32());
+            let b = Wrapping(samples[1].to_i32());
+            let c = Wrapping(samples[2].to_i32());
+            let d;
+            let e;
+            let f;
+            if ty == 6 {
+                let tmp = a - (c >> 1);
+                e = c + tmp;
+                f = tmp - (b >> 1);
+                d = f + b;
+            } else {
+                d = a;
+                f = if ty & 1 != 0 { c + a } else { c };
+                e = if (ty >> 1) == 1 {
+                    b + a
+                } else if (ty >> 1) == 2 {
+                    b + ((a + f) >> 1)
+                } else {
+                    b
+                };
+            }
+            *samples[permutation % 3] = S::from_i32(d.0);
+            *samples[(permutation + 1 + (permutation / 3)) % 3] = S::from_i32(e.0);
+            *samples[(permutation + 2 - (permutation / 3)) % 3] = S::from_i32(f.0);
         }
     }
 }
