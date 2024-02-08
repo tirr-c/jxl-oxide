e0d28af3fac0435c35530c0a37a2334e380a1380
Wonwoo Choi <chwo9843@gmail.com>
2024-02-08 16:41:11 +0900

    jxl-coding: Detect invalid LZ77 stream (#217)
    * Move tracing to workspace dependency
    
    * jxl-coding: Detect invalid LZ77 stream

--- crates/jxl-coding/src/error.rs
+++ crates/jxl-coding/src/error.rs
@@ -8,6 +8,7 @@ pub enum Error {
     InvalidIntegerConfig,
     InvalidPermutation,
     InvalidPrefixHistogram,
+    UnexpectedLz77Repeat,
 }
 
 impl std::error::Error for Error {
@@ -29,6 +30,10 @@ impl std::fmt::Display for Error {
             Self::InvalidIntegerConfig => write!(f, "invalid hybrid integer configuration"),
             Self::InvalidPermutation => write!(f, "invalid permutation"),
             Self::InvalidPrefixHistogram => write!(f, "invalid Brotli prefix code"),
+            Self::UnexpectedLz77Repeat => write!(
+                f,
+                "LZ77 repeat symbol encountered without decoding any symbols"
+            ),
         }
     }
 }
