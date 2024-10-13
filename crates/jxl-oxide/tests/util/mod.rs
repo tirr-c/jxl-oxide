943c46ffc1fd535ac11da1317e847247003b8120
Wonwoo Choi <chwo9843@gmail.com>
2024-10-13 20:15:35 +0900

    Run conformance tests in nix build

--- crates/jxl-oxide/tests/util/mod.rs
+++ crates/jxl-oxide/tests/util/mod.rs
@@ -1,8 +1,21 @@
 #![allow(dead_code)]
 
-pub fn conformance_path(name: &str) -> std::path::PathBuf {
-    let mut path = std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    path.push("tests/conformance/testcases");
+use std::path::PathBuf;
+
+fn testcases_dir() -> PathBuf {
+    let testcases_env = std::env::var_os("JXL_OXIDE_TESTCASES");
+    match testcases_env {
+        Some(testcases_dir) if !testcases_dir.is_empty() => PathBuf::from(testcases_dir),
+        _ => {
+            let mut testcases_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+            testcases_dir.push("tests/conformance/testcases");
+            testcases_dir
+        }
+    }
+}
+
+pub fn conformance_path(name: &str) -> PathBuf {
+    let mut path = testcases_dir();
     path.push(name);
     path.push("input.jxl");
     path
