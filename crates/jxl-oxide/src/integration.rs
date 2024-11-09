e58b89f81c6131ff262fd8a4e4e29f4cc8bb5928
Wonwoo Choi <chwo9843@gmail.com>
2024-11-09 17:06:19 +0900

    jxl-oxide: Add more docs around the `image` integration (#383)

--- crates/jxl-oxide/src/integration.rs
+++ crates/jxl-oxide/src/integration.rs
@@ -1,4 +1,12 @@
+//! Various integrations to other library crates.
+//!
+//! # Available integrations
+//!
+//! Integrations are enabled with feature flags.
+//! - `JxlDecoder`, which implements `image::ImageDecoder` (`image` feature)
+
 #[cfg(feature = "image")]
 mod image;
 
+#[cfg(feature = "image")]
 pub use image::*;
