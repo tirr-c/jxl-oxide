9935f8147b99e6570b704e2e2adcf597b0092585
Wonwoo Choi <chwo9843@gmail.com>
2026-08-16 23:50:07 +0900

    Do adaptive LF smoothing for each MCU

--- crates/jxl-render/src/vardct/generic/mod.rs
+++ crates/jxl-render/src/vardct/generic/mod.rs
@@ -117,38 +117,51 @@ fn adaptive_lf_smoothing_subsampled_impl(
     height: usize,
     shifts: [ChannelShift; 3],
     in_xyb: [&mut [f32]; 3],
-    [lf_x, lf_y, lf_b]: [f32; 3],
+    lf_xyb: [f32; 3],
     tracker: Option<&AllocTracker>,
 ) -> crate::Result<()> {
+    let (unit_w, unit_h) = {
+        let ChannelShift::JpegUpsampling {
+            has_h_subsample,
+            has_v_subsample,
+            ..
+        } = shifts[0]
+        else {
+            panic!("expected ChannelShift::JpegUpsampling");
+        };
+        (
+            if has_h_subsample { 2 } else { 1 },
+            if has_v_subsample { 2 } else { 1 },
+        )
+    };
+    assert!(width.is_multiple_of(unit_w));
+    assert!(height.is_multiple_of(unit_h));
+    if width / unit_w <= 2 || height / unit_h <= 2 {
+        return Ok(());
+    }
+
+    let unit_sw = shifts.map(|shift| unit_w >> shift.vshift());
+    let unit_sh = shifts.map(|shift| unit_h >> shift.hshift());
+
     let dims = shifts.map(|shift| {
         let (w, h) = shift.shift_size((width as u32, height as u32));
         (w as usize, h as usize)
     });
-    for (w, h) in dims {
-        if w <= 2 || h <= 2 {
-            return Ok(());
-        }
-    }
 
     for ((w, h), in_lf) in std::iter::zip(dims, &in_xyb) {
         assert_eq!(in_lf.len(), w * h);
     }
 
-    let [in_x, in_y, in_b] = in_xyb;
+    let wa_x = AlignedGrid::with_alloc_tracker(dims[0].0 - 2, dims[0].1 - 2, tracker)?;
+    let wa_y = AlignedGrid::with_alloc_tracker(dims[1].0 - 2, dims[1].1 - 2, tracker)?;
+    let wa_b = AlignedGrid::with_alloc_tracker(dims[2].0 - 2, dims[2].1 - 2, tracker)?;
+    let mut wa = [wa_x, wa_y, wa_b];
 
-    let mut wa_x = AlignedGrid::with_alloc_tracker(dims[0].0 - 2, dims[0].1 - 2, tracker)?;
-    let mut wa_y = AlignedGrid::with_alloc_tracker(dims[1].0 - 2, dims[1].1 - 2, tracker)?;
-    let mut wa_b = AlignedGrid::with_alloc_tracker(dims[2].0 - 2, dims[2].1 - 2, tracker)?;
-
-    for (width, g, out) in [
-        (dims[0].0, &mut *in_x, wa_x.buf_mut()),
-        (dims[1].0, &mut *in_y, wa_y.buf_mut()),
-        (dims[2].0, &mut *in_b, wa_b.buf_mut()),
-    ] {
-        let up = g.chunks_exact(width);
-        let center = g[width..].chunks_exact(width);
-        let down = g[width * 2..].chunks_exact(width);
-        let out = out.chunks_exact_mut(width - 2);
+    for ((out, g), (sw, _)) in wa.iter_mut().map(|i| i.buf_mut()).zip(&in_xyb).zip(dims) {
+        let up = g.chunks_exact(sw);
+        let center = g[sw..].chunks_exact(sw);
+        let down = g[sw * 2..].chunks_exact(sw);
+        let out = out.chunks_exact_mut(sw - 2);
         for (((up, center), down), out) in up.zip(center).zip(down).zip(out) {
             for (((u, c), d), out) in up
                 .windows(3)
@@ -164,66 +177,55 @@ fn adaptive_lf_smoothing_subsampled_impl(
         }
     }
 
-    let mut last_coords_y = [0; 3];
-    let mut current_row_x = vec![0.0; dims[0].0];
-    let mut current_row_y = vec![0.0; dims[1].0];
-    let mut current_row_b = vec![0.0; dims[2].0];
-    for row in 0..height {
-        let coords_y = shifts.map(|shift| row >> shift.vshift());
-        if std::iter::zip(coords_y, dims).any(|(row, (_, height))| row == 0 || row == height - 1) {
-            continue;
-        }
+    for sy in 1..(height / unit_h - 1) {
+        let y = sy * unit_h;
+        for sx in 1..(width / unit_w - 1) {
+            let x = sx * unit_w;
+            let mut gap = 0.5f32;
+            for ch in 0..3 {
+                let shift = shifts[ch];
+                let sry = y >> shift.vshift();
+                let srx = x >> shift.hshift();
+                let sw = dims[ch].0;
+                let wa = &wa[ch];
+                let lf_factor = lf_xyb[ch];
 
-        let row_x = &mut in_x[coords_y[0] * dims[0].0..][..dims[0].0];
-        let row_y = &mut in_y[coords_y[1] * dims[1].0..][..dims[1].0];
-        let row_b = &mut in_b[coords_y[2] * dims[2].0..][..dims[2].0];
+                for dy in 0..unit_sh[ch] {
+                    let y = sry + dy;
+                    let row = &in_xyb[ch][y * sw..][..sw];
 
-        if last_coords_y[0] != coords_y[0] {
-            current_row_x.copy_from_slice(row_x);
-        }
-        if last_coords_y[1] != coords_y[1] {
-            current_row_y.copy_from_slice(row_y);
-        }
-        if last_coords_y[2] != coords_y[2] {
-            current_row_b.copy_from_slice(row_b);
-        }
-        last_coords_y = coords_y;
+                    for dx in 0..unit_sw[ch] {
+                        let x = srx + dx;
 
-        for col in 0..width {
-            let coords_x = shifts.map(|shift| col >> shift.hshift());
-            if std::iter::zip(coords_x, dims).any(|(col, (width, _))| col == 0 || col == width - 1)
-            {
-                continue;
+                        let me = row[x];
+                        let wa = wa.get(x - 1, y - 1);
+                        let gap_t = (wa - me).abs() / lf_factor;
+
+                        gap = gap.max(gap_t);
+                    }
+                }
             }
 
-            let skip_update: [_; 3] = std::array::from_fn(|idx| {
-                let shift = shifts[idx];
-                (coords_y[idx] << shift.vshift()) != row || (coords_x[idx] << shift.hshift()) != col
-            });
-
-            let x_self = current_row_x[coords_x[0]];
-            let x_wa = wa_x.get(coords_x[0] - 1, coords_y[0] - 1);
-            let x_gap_t = (x_wa - x_self).abs() / lf_x;
-
-            let y_self = current_row_y[coords_x[1]];
-            let y_wa = wa_y.get(coords_x[1] - 1, coords_y[1] - 1);
-            let y_gap_t = (y_wa - y_self).abs() / lf_y;
-
-            let b_self = current_row_b[coords_x[2]];
-            let b_wa = wa_b.get(coords_x[2] - 1, coords_y[2] - 1);
-            let b_gap_t = (b_wa - b_self).abs() / lf_b;
-
-            let gap = 0.5f32.max(x_gap_t).max(y_gap_t).max(b_gap_t);
             let gap_scale = (3.0 - 4.0 * gap).max(0.0);
+            for ch in 0..3 {
+                let shift = shifts[ch];
+                let sry = y >> shift.vshift();
+                let srx = x >> shift.hshift();
+                let sw = dims[ch].0;
+                let wa = &wa[ch];
 
-            if !skip_update[0] {
-                row_x[coords_x[0]] = (x_wa - x_self) * gap_scale + x_self;
-            }
-            if !skip_update[1] {
-                row_y[coords_x[1]] = (y_wa - y_self) * gap_scale + y_self;
-            }
-            if !skip_update[2] {
-                row_b[coords_x[2]] = (b_wa - b_self) * gap_scale + b_self;
+                for dy in 0..unit_sh[ch] {
+                    let y = sry + dy;
+                    let row = &mut in_xyb[ch][y * sw..][..sw];
+
+                    for dx in 0..unit_sw[ch] {
+                        let x = srx + dx;
+
+                        let me = row[x];
+                        let wa = wa.get(x - 1, y - 1);
+                        row[x] = (wa - me) * gap_scale + me;
+                    }
+                }
             }
         }
     }
